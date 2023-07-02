bff58ab010fe3619777830fbff15bcc3478ad684
Travis <tmann1794@gmail.com>
2023-07-02 17:11:45 -0400

    Renamed Views, Improved Get

---
+++ controller/log/script_reciever.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""script_receiver.py: intermediary between script and log output for thread safe data transfer"""
+
+
+# --- metadata ---
+__author__ = "Travis Mann"
+__version__ = "0.1"
+__maintainer__ = "Travis Mann"
+__email__ = "tmann.eng@gmail.com"
+__status__ = "Development"
+
+
+# --- imports ---
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+
+
+# --- classes ---
+# https://stackoverflow.com/questions/21071448/redirecting-stdout-and-stderr-to-a-pyqt4-qtextedit-from-a-secondary-thread
+class ScriptReceiver(QObject):
+    textReceived = pyqtSignal(str)
+
+    def __init__(self,queue,*args,**kwargs):
+        QObject.__init__(self,*args,**kwargs)
+        self.queue = queue
+
+    @pyqtSlot()
+    def run(self):
+        while True:
+            text = self.queue.get()
+            self.textReceived.emit(text)
--- controller/run_controller.py
+++ controller/run_controller.py
@@ -13,9 +13,15 @@ __status__ = "Development"
 
 
 # --- imports ---
+from selenium_metaprogramming.controller.script_worker import ScriptWorker
+from selenium_metaprogramming.controller.log.script_reciever import ScriptReceiver
+from selenium_metaprogramming.controller.log.queue_stream import WriteStream
+
+import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QThread
-from selenium_metaprogramming.script_worker import ScriptWorker
+from PyQt5.QtGui import QTextCursor
+from queue import Queue
 
 
 # --- main ---
@@ -26,7 +32,6 @@ class RunController:
         # connect build view elements to actions
         self.main_window.run_view.run_button.clicked.connect(self.run_script)
         self.main_window.run_view.build_button.clicked.connect(self.to_build_view)
-        self.main_window.run_view.cancel_button.clicked.connect(self.stop_script)
 
     def to_build_view(self):
         """
@@ -43,51 +48,128 @@ class RunController:
         print('stopping script')
 
         # disable multiple cancel inputs
-        self.main_window.run_view.cancel_button.setEnabled(False)
+        self.main_window.run_view.run_button.setEnabled(False)
 
         # kill thread
         if self.thread.isRunning():
             self.thread.exit()
             print('script stopped')
 
+    def get_cancel_button(self):
+        """
+        purpose: switch run button for cancel button
+        """
+        print('switching run for cancel'),
+        self.main_window.run_view.run_button.setEnabled(False),  # disable button before switching function
+        self.main_window.run_view.run_button.setText("Cancel"),  # update text to new button function
+        self.main_window.run_view.run_button.clicked.disconnect(),  # disconnect script run action
+        self.main_window.run_view.run_button.clicked.connect(self.stop_script),  # connect new button action
+        self.main_window.run_view.run_button.setEnabled(True),  # re-enable button
+
+    def get_run_button(self):
+        """
+        purpose: switch cancel button for run button
+        """
+        print('switching cancel for run'),
+        self.main_window.run_view.run_button.setEnabled(False),  # disable button before switching function
+        self.main_window.run_view.run_button.setText(f"Run {self.script.name}"),  # update button text to run action
+        self.main_window.run_view.run_button.clicked.disconnect(),  # disconnect script cancel action
+        self.main_window.run_view.run_button.clicked.connect(self.run_script),  # connect new button action
+        self.main_window.run_view.run_button.setEnabled(True),  # re-enable button
+
+    def prepare_script_log(self):
+        """
+        purpose: handle moving stdout from the script to the log output
+        """
+        # log action
+        print('preparing script log')
+
+        # set up worker and thread
+        queue = Queue()
+        sys.stdout = WriteStream(queue)
+        self.reciever_thread = QThread()
+        self.reciever = ScriptReceiver(queue)
+
+        # connect start and end signals
+        self.reciever_thread.started.connect(self.reciever.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        # connect actions
+        self.reciever.textReceived.connect(lambda text: [
+                                         print(text),
+                                         self.main_window.run_view.log_output.moveCursor(QTextCursor.End),
+                                         self.main_window.run_view.log_output.insertPlainText(text),
+                                     ])
+
+        # prepare thread
+        self.reciever.moveToThread(self.reciever_thread)
+
+    def end_script_log(self):
+        """
+        purpose: kill receiver and thread
+        """
+        print('ending script log')
+        self.reciever_thread.quit()
+        self.reciever.deleteLater()
+        self.reciever_thread.deleteLater()
+
     def run_script(self):
         """
         purpose: run currently built script
         """
         # get script
-        script = self.main_window.build_controller.script
+        self.script = self.main_window.build_controller.script
 
         # create a script worker and child thread
         self.thread = QThread()
-        self.worker = ScriptWorker(script)
+        self.worker = ScriptWorker(self.script)
         self.worker.moveToThread(self.thread)
 
-        # connect signals and slots
-        self.thread.started.connect(self.worker.run_script)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
+        # prepare text receiver thread
+        self.prepare_script_log()
 
-        # disable/ enable buttons with script start and finish
+        # connect signals and slots
+        # connect thread start actions
+        print('connecting worker thread start actions')
         self.thread.started.connect(
             lambda: [
-            self.main_window.run_view.run_button.setEnabled(False),
-            self.main_window.run_view.build_button.setEnabled(False),
-            self.main_window.run_view.cancel_button.setEnabled(True),
+                # disable switching to build view
+                self.main_window.run_view.build_button.setEnabled(False),
+                # switch run button to cancel button
+                self.get_cancel_button(),
+                # start log output receiver
+                self.reciever_thread.start(),
+                # start script worker
+                self.worker.run_script(),
             ]
         )
 
-        # connect resets after script complete
+        print('connecting worker finished actions')
+        # end thread and destroy worker when done
         self.thread.finished.connect(
             lambda: [
-            self.main_window.run_view.run_button.setEnabled(True),
-            self.main_window.run_view.build_button.setEnabled(True),
-            self.main_window.run_view.cancel_button.setEnabled(False),
+                self.thread.quit(),
+                self.worker.deleteLater(),
+            ]
+        )
+
+        print('connecting worker thread finished actions')
+        # reset gui after thread is done
+        self.thread.finished.connect(
+            lambda: [
+                # delete thread
+                self.thread.deleteLater(),
+                # re-enable switching to build view
+                self.main_window.run_view.build_button.setEnabled(True),
+                # switch cancel button back to run button
+                self.get_run_button(),
+                # end log receiver
+                self.end_script_log()
             ]
         )
 
         # start child thread
-        print('starting child thread')
+        print('starting worker thread')
         self.thread.start()
-        print(self.thread.isRunning())
-        print('child thread done')
